67f58497346377c906bfddebe663132aaa7a0ddd
Saski Amalia Hastuti <saskiiamalia@gmail.com>
2016-10-29 14:56:55 +0700

    Intent SMS

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -16,6 +16,15 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
     }
 
+    public void composeSmsMessage(String message)
+    {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain" );
+        intent.putExtra("sms_body", message);
+        if(intent.resolveActivity(getPackageManager()) !=null)
+            startActivity(intent);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +37,16 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(View v)
                     {
                         dialPhoneNumber("0341712500");
+                    }
+                });
+
+        findViewById(R.id.imageViewSMS)
+                .setOnClickListener(new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        composeSmsMessage("Pesan dari SMK Telkom Malang");
                     }
                 });
     }
